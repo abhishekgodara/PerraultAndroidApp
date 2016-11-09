d35909868f1f065e82383cd3de5ba748ba0effd1
tarunmittal21 <tarunmittal.itbhu@gmail.com>
2016-11-09 22:51:57 +0530

    0.1

---
+++ app/src/main/java/com/perraulthealth/User.java
@@ -0,0 +1,21 @@
+package com.perraulthealth;
+
+/**
+ * Created by sutu on 11/9/2016.
+ */
+
+public class User {
+
+    private Integer mobile;
+
+    public User() {
+    }
+
+    public Integer getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(Integer mobile) {
+        this.mobile = mobile;
+    }
+}
